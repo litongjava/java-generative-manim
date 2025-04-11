11aa2ad6f83cb4789fc2836ccf74cd155fe8610a
litongjava <litongjava@qq.com>
2025-04-10 17:45:54 -1000

    update code example

--- src/test/java/com/litongjava/manim/services/ManimVideoServiceTest.java
+++ src/test/java/com/litongjava/manim/services/ManimVideoServiceTest.java
@@ -7,12 +7,20 @@ import com.litongjava.manim.config.AdminAppConfig;
 import com.litongjava.manim.vo.ExplanationVo;
 import com.litongjava.tio.boot.testing.TioBootTest;
 
-public class MainimServiceTest {
+public class ManimVideoServiceTest {
 
   @Test
-  public void test() {
+  public void test_Kjeldahl() {
     TioBootTest.runWith(AdminAppConfig.class);
     ExplanationVo explanationVo = new ExplanationVo("Chemistry - Kjeldahl Method");
+    Aop.get(MainimService.class).index(explanationVo, false, null);
+  }
+
+  @Test
+  public void test_light() {
+    TioBootTest.runWith(AdminAppConfig.class);
+    ExplanationVo explanationVo = new ExplanationVo("generate a video about refraction of light");
+    explanationVo.setLanguage("English");
     Aop.get(MainimService.class).index(explanationVo, false, null);
   }
 
@@ -25,6 +33,7 @@ public class MainimServiceTest {
 
   @Test
   public void getSystemPrompt() {
+    TioBootTest.runWith(AdminAppConfig.class);
     String systemPrompt = Aop.get(MainimService.class).getSystemPrompt();
     System.out.println(systemPrompt);
   }
